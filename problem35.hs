fc06656125677f5af93f3aa6ad55a222e05a45c8
George Ehrhorn <ehrhorn@gmail.com>
2013-03-29 22:58:35 -0400

    Testing algo

--- problem35.hs
+++ problem35.hs
@@ -4,10 +4,18 @@ factors n = [x | x <- [1..n], mod n x == 0]
 prime :: Int -> Bool
 prime n = factors n == [1,n]
 
-digs :: Integral x => x -> [x]
-digs 0 = []
-digs x = digs (div x 10) ++ [mod x 10]
+genCircles :: String -> [String] 
+genCircles [] = []
+genCircles x = take (length x) (iterate (rotate) x)
 
-rotateDigs :: [Int] -> [Int]
-rotateDigs [] = []
-rotateDigs x = tail x ++ (head x):[] 
+int2string :: Integer -> String
+int2string x = show x
+
+rotate :: String -> String
+rotate [] = []
+rotate x = tail x ++ [head x]
+
+intList :: [String] -> [Int]
+intList = map read
+
+-- run [n | n <- [2, 3..1000000], all prime(intList (genCircls (int2stringn)))]
