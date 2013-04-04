4bd0619009175e6d52957ca84aa6b63f71115e4c
George Ehrhorn <ehrhorn@gmail.com>
2013-04-04 16:14:23 -0400

    Refactoring

--- problem22.hs
+++ problem22.hs
@@ -3,9 +3,8 @@ import Data.Char
 
 main = do 
   fileContents <- readFile "problem22/names.txt"
-  let names = lines fileContents
-  let sortedNames = sort names
-  let zippedNames = zip sortedNames [1..]
+  let names = sort $ lines fileContents
+  let zippedNames = zip names [1..]
   let finalScore = map calcNameScore zippedNames
   print (sum finalScore)
   return ()
