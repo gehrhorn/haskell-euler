a889f14f369ad49f411b1fdd26dbf5e51dec9fdc
George Ehrhorn <ehrhorn@gmail.com>
2013-04-03 22:39:39 -0400

    Initial solutions of prob 21. Run as sum $ listAmicables 10000

--- problem21.hs
+++ problem21.hs
@@ -2,3 +2,7 @@ factors :: (Integral x) => x -> [x]
 factors x = [i | i <- [1..x], mod x i == 0]
 
 d = sum . init . factors
+
+listAmicables :: Int -> [Int]
+listAmicables x = [a | a <- [2..x], a == (d $ d a), a /= d a]
+  
