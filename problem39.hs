2b8b3d886d7eec3ba5f9bae58b16c4ec49e37c0f
George Ehrhorn <ehrhorn@gmail.com>
2013-04-04 20:31:00 -0400

    Testing

---
+++ problem39.hs
@@ -0,0 +1,6 @@
+genRightTriangles :: Int -> [(Int, Int, Int)] 
+genRightTriangles p = [ (a,b,c) | a <- [1..p], b <- [a..p], c <- [b..p], a+b+c == p, a^2 + b^2 == c^2]
+
+main = do
+  let tris = [genRightTriangles i | i <- [3..1000]] 
+  print  ( take 50 tris )
