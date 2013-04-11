77b60fd46007ddb2ef969d4bcf1d673ae2ed042a
George Ehrhorn <ehrhorn@gmail.com>
2013-04-10 21:32:05 -0400

    Testing iterating

--- problem39.hs
+++ problem39.hs
@@ -2,5 +2,5 @@ genRightTriangles :: Int -> [(Int, Int, Int)]
 genRightTriangles p = [ (a,b,c) | a <- [1..p], b <- [a..p], c <- [b..p], a+b+c == p, a^2 + b^2 == c^2]
 
 main = do
-  let tris = [genRightTriangles i | i <- [3..1000]] 
+  let tris = [genRightTriangles i | i <- [4,6..1000]] 
   print  ( take 50 tris )
